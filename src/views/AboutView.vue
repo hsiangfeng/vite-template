bbea0d9d396f018ed4d916159434b244f66d8663
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2024-01-16 10:17:30 +0800

    feat: 🎸 firest

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>This is About page.</h1>
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>Hello, This is Home Page.</h1>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <hr>
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style scoped>
+</style>
