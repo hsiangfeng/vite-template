94acfb3e59b32b193efa939791d7f0ae7be8dde8
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2024-01-16 10:17:50 +0800

    Updates

---
+++ assets/AboutView-Fekv0v4I.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,e as o,w as a,f as e,F as r,b as c,r as _,o as u}from"./index-nNXcROrU.js";const l={},i=c("h1",null,"This is About page.",-1);function p(d,f,m,b,h,x){const t=_("router-link");return u(),n(r,null,[i,o(t,{to:"/"},{default:a(()=>[e("Home")]),_:1}),e(" | "),o(t,{to:"/about"},{default:a(()=>[e("About")]),_:1})],64)}const V=s(l,[["render",p]]);export{V as default};
